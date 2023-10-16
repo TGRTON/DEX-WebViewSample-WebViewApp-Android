9e61d8bd6051257e7f61f59c065b0db0c2cd8624
Jason Gatsby <3163941+DeFiTON@users.noreply.github.com>
2023-10-16 07:12:19 +0300

    Add files via upload

---
+++ WebViewSample/app/src/main/java/com/tergofinanceall/financetergo/MyFirebaseMessagingService.kt
@@ -0,0 +1,9 @@
+package com.tergofinanceall.financetergo
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+}
